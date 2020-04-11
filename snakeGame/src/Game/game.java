11dfe151bbaf6cdfdc1bd41448c3f6e3cf875da6
siinatra35 <50811648+siinatra35@users.noreply.github.com>
2020-04-11 14:08:41 -0500

    updating snake obj

--- snakeGame/src/Game/game.java
+++ snakeGame/src/Game/game.java
@@ -1,36 +1,69 @@
 package Game;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import javafx.scene.control.Label;
+
+
+import java.util.Random;
 
 public class game extends Application {
-    private Label label;
+    private Pane root = new Pane();
+    private Rectangle[] rectangle = new Rectangle[100];
+    private Rectangle food;
+    private Random rand = new Random();
 
+    private Rectangle generateShape() {
+        //size of food/snake
+        Rectangle foodAndSnake = new Rectangle(45, 45);
+        //color of snake/food
+        foodAndSnake.setFill(Color.BROWN);
+        //outline of snake/food
+        foodAndSnake.setStroke(Color.BLACK);
+        //allows snake to pass border
+        foodAndSnake.setVisible(false);
+        return foodAndSnake;
+
+
+    }
+
+    /**
+     *
+     * @param primaryStage
+     */
     @Override
     public void start(Stage primaryStage) {
 
-        Button startButton = new Button("Start game");
-        startButton.setOnAction(new StartButtonHandler());
-        Button exitGame = new Button("Quit Game");
-        Label intructions = new Label("Use WASD to move snake");
+        //@TODO create a game control in a t form for better visual
+        GridPane gridPane = new GridPane();
 
-        //lamda expression to handle quiting game
-        exitGame.setOnAction(event -> {
-            System.exit(0);//exits game
-        });
-        HBox hBox = new HBox(10, startButton, exitGame);
-        VBox vBox = new VBox(hBox,intructions);
-      //  hBox.setAlignment(Pos.BOTTOM_CENTER);
-        vBox.setAlignment(Pos.BASELINE_CENTER);
-        Scene scene = new Scene(vBox);
+
+        //button objects for moving snake
+        Button up = new Button("up");
+        Button down = new Button("down");
+        Button left = new Button("left");
+        Button right = new Button("right");
+
+        //getting Rectangle form generateShape method
+        food = generateShape();
+        food.setStroke(Color.RED);
+        food.setVisible(true);
+        food.setTranslateX(rand.nextInt(10) * 50);
+        food.setTranslateY(rand.nextInt(10) * 50);
+      //  root.getChildren().add(food);
+
+
+
+        HBox hBox = new HBox(10, food, left, up, down, right);
+        hBox.setAlignment(Pos.BOTTOM_CENTER);
+        Scene scene = new Scene(hBox);
         primaryStage.setScene(scene);
         primaryStage.setMinHeight(543);
         primaryStage.setMinWidth(514);
@@ -40,14 +73,10 @@ public class game extends Application {
 
     }
 
-    class StartButtonHandler implements
-            EventHandler<ActionEvent> {
-        @Override
-        public void handle(ActionEvent actionEvent) {
-
-        }
-    }
-
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         launch(args);
 
